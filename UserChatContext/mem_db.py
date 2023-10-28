c179c1cfdfbaa8b85344346111db371dec2844c9
Amartya <amartya.3654@gmail.com>
2023-10-28 20:40:26 +0530

    search context added via in memory DB, update readme.md

---
+++ UserChatContext/mem_db.py
@@ -0,0 +1,23 @@
+from Utils.helpers import mergeContext
+class UserChatClient:
+    def __init__(self) -> None:
+        self.client = None
+    
+    def getClient(self):
+        if self.client is None:
+            self.client = dict()
+        return self.client
+    
+    def getContext(self, userId):
+        if userId not in self.client.keys():
+            self.client.setdefault(userId, dict())
+        return self.client[userId]
+    
+    def addContext(self, userId, context):
+        self.client[userId] = mergeContext(self.getContext(userId), context)
+        return self.client[userId]
+    
+    def removeContext(self, userId):
+        self.client[userId] = dict()
+
+userChatClient = UserChatClient()
